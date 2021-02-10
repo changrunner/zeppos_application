4dd995e8116a88086b1a706bb02d92bad77087c3
Haegdorens, Gert <gert.haegdorens@yrcw.com>
2021-02-10 11:30:46 -0600

    Add Method config_values_for_app. Add debug statements. Deploy 0.0.7

--- tests/test_app_config.py
+++ tests/test_app_config.py
@@ -1,10 +1,18 @@
 import unittest
 from zeppos_application.app_config import AppConfig
+from zeppos_logging.app_logger import AppLogger
 
 class TestTheProjectMethods(unittest.TestCase):
     def test_get_json_config_dict_methods(self):
+        # AppLogger.configure_and_get_logger("test")
+        # AppLogger.set_debug_level()
         self.assertEqual("123", AppConfig.get_json_config_dict(__file__)["test_me"])
         self.assertEqual(None, AppConfig.get_json_config_dict(__file__, "config_not_there.json"))
+
+    def test_config_values_for_app_methods(self):
+        # AppLogger.configure_and_get_logger("test")
+        # AppLogger.set_debug_level()
+        self.assertEqual('main', AppConfig.config_values_for_app('tests')["environment"])
 
 
 if __name__ == '__main__':
--- zeppos_application/app_config.py
+++ zeppos_application/app_config.py
@@ -1,18 +1,23 @@
 from zeppos_root.root import Root
-from os import path
 from json import loads
-from cachetools import cached, LRUCache, TTLCache
+from cachetools import cached, TTLCache
+from zeppos_logging.app_logger import AppLogger
+from os import path, getcwd
+from zeppos_git.branch import Branch
+
 
 class AppConfig:
-
     @staticmethod
     # cache data for no longer than ten minutes
     @cached(cache=TTLCache(maxsize=1024, ttl=600))
     def get_json_config_dict(current_module_filename, config_file_name="config.json"):
+        AppLogger.logger.debug(f"current_module_filename: {current_module_filename}")
+        AppLogger.logger.debug(f"config_file_name: {config_file_name}")
         root_path = Root.find_root_of_project(
                     current_module_filename=current_module_filename,
                     root_marker_filename_list=[config_file_name]
                 )
+        AppLogger.logger.debug(f"root_path: {root_path}")
         if root_path and path.exists(root_path):
             config_full_file_name = \
                 path.join(
@@ -21,8 +26,31 @@ class AppConfig:
                 )
             if path.exists(config_full_file_name):
                 with open(config_full_file_name, 'r') as fl:
-                    return loads(fl.read())
-
+                    config_values = loads(fl.read())
+                    AppLogger.logger.debug(f"Config Values: {config_values}")
+                    return config_values
         return None
+
+    @staticmethod
+    # cache data for no longer than ten minutes
+    @cached(cache=TTLCache(maxsize=1024, ttl=600))
+    def config_values_for_app(app_dir):
+        AppLogger.logger.debug("Get Configuration")
+        AppLogger.logger.debug(f"__file__: {__file__}")
+        AppLogger.logger.debug(f"Root: {Root.find_root_of_project(__file__)}")
+        AppLogger.logger.debug(f"current_module_filename: {path.join(Root.find_root_of_project(__file__), app_dir)}")
+        AppLogger.logger.debug(f"config_file_name: config.{Branch.get_current()}.json")
+
+        config_dict = AppConfig.get_json_config_dict(
+            current_module_filename=path.join(Root.find_root_of_project(__file__), app_dir),
+            config_file_name=f"config.{Branch.get_current()}.json"
+        )
+
+        if config_dict:
+            AppLogger.logger.debug(f"Environment: {config_dict['environment']}")
+        else:
+            AppLogger.logger.error(f"There is no config [config.{Branch.get_current()}.json] probably! "
+                                   f"The branch should be local, test, production.")
+        return config_dict
 
 
